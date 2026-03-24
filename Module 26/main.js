d388dce5218986dfc4c1d6c1d289ef2f10c8280e
tiarasabedinajj <tiarasabedinaj51@gmail.com>
2026-03-24 19:21:03 +0100

    module 26

---
+++ Module 26/main.js
@@ -0,0 +1,41 @@
+console.log("HELLO!")
+
+var x=5 * 55555
+
+console.log(x);
+
+
+var a=50
+var b=10
+var c=a-b
+console.log(c);
+
+
+var n=100;
+n++;
+console.log(n);
+
+var t=100;
+t--;
+console.log(t)
+
+var b=100;
+var o=b**2
+console.log(o)
+    
+var y=15;
+y= 5;
+console.log(y);
+
+var b1= 555;
+var b5="111";
+
+console.log(b1>b5);
+console.log(b1<b5);
+console.log(b1==b5);
+
+var name = "edra";
+var surname = "ledri";
+
+console.log(name+surname)
+
