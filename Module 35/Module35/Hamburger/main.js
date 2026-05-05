ce1a91044285441d6555f8ca0f7f8a7e028a4d6b
tiarasabedinajj <tiarasabedinaj51@gmail.com>
2026-05-05 19:10:15 +0200

    module35

---
+++ Module 35/Module35/Hamburger/main.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    var menu = document.getElementById("myLinks");
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display ="block";
+    }
+}
---
+++ Module 35/Module35/slideshow/main.js
@@ -0,0 +1,27 @@
+var slideIndex=1;
+
+function showSlides(n){
+var slides = document.getElementById("slide");
+
+for(i=0; i <slides.length;i++){
+    slides[i].style.display = "none";
+}
+
+if(n > slides.length){
+    slideIndex = 1;
+}
+
+if(n<1){
+    slideIndex = slides.length;
+}
+
+slides[slideIndex -1].style.display = "block";
+}
+
+showSlides(1);
+function plusSlides(n){
+    slideIndex += n;
+    showSlides(slideIndex);
+}
+
+setInterval(plusSlides,3000,1);
